525e27806135600dd831ce10b370867f88f864d9
Sélénuix <lrtcfr1@gmail.com>
2017-03-18 18:21:02 +0100

    Fix bugs

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # Installation des logiciels
+cd $HOME
 
 sudo add-apt-repository -y ppa:varlesh-l/indicator-kdeconnect
-sudo apt-key -y add --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list 
 sudo apt-get update
 
-cd $HOME
 sudo apt-get install -y kdeconnect indicator-kdeconnect apache2 libapache2-mod-php7.0 \
 	mysql-server mysql-client php7.0 phpmyadmin \
 	spotify-client xfce4-screenshooter \
@@ -13,10 +13,6 @@ sudo apt-get install -y kdeconnect indicator-kdeconnect apache2 libapache2-mod-p
 
 wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
 wget https://dl.discordapp.net/apps/linux/0.0.1/discord-0.0.1.deb dpkg -i sublime-text_build-3126_amd64.deb dpkg -i discord-0.0.1.deb
-rm sublime-text_build-3126_amd64.deb rm discord-0.0.1.deb
+rm sublime-text_build-3126_amd64.deb 
+rm discord-0.0.1.deb
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-# Installation des fonts
-git clone git@github.com:abertsch/Menlo-for-Powerline.git
-cd Menlo-for-Powerline/
-cp "Menlo for Powerline.ttf" ~/.fonts fc-cache -vf ~/.fonts
